c8810d775ed750db01aa5e86d90f804aea1823eb
sevenlol <sevenlol1007@gmail.com>
2016-04-07 00:43:59 +0800

    [master] Add getNiciProject interface in NiciClient and Add fake data in the factory for testing

--- app/src/main/java/tw/gov/ey/nici/network/NiciClient.java
+++ app/src/main/java/tw/gov/ey/nici/network/NiciClient.java
@@ -3,10 +3,14 @@ package tw.gov.ey.nici.network;
 import java.util.List;
 
 import tw.gov.ey.nici.models.NiciInfo;
+import tw.gov.ey.nici.models.NiciProject;
 
 public interface NiciClient {
     /* other methods */
     void clearCache();
+
+    /* NiciProject */
+    NiciProject getNiciProject();
 
     /* NiciInfo */
     int getNiciInfoCount();
