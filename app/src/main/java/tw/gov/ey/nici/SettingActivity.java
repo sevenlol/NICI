73e151a7b3af3961fea695dd9756d8e1a8e6e0e7
sevenlol <sevenlol1007@gmail.com>
2016-04-09 23:46:50 +0800

    [master] Implement setting page (add scroll detection and display choice preference)

--- app/src/main/java/tw/gov/ey/nici/SettingActivity.java
+++ app/src/main/java/tw/gov/ey/nici/SettingActivity.java
@@ -2,6 +2,7 @@ package tw.gov.ey.nici;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -27,11 +28,26 @@ public class SettingActivity extends AppCompatActivity implements View.OnClickLi
         if (actionBar != null) {
             actionBar.setNavigationOnClickListener(this);
         }
+
+        // set preference fragment
+        if (getSupportFragmentManager().findFragmentById(R.id.setting_main_fragment) == null) {
+            getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.setting_main_fragment, new Preference())
+                .commit();
+        }
     }
 
     @Override
     public void onClick(View view) {
         // TODO save settings
         finish();
+    }
+
+    public static class Preference extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle bundle, String s) {
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
