a4cc71cc9cb3e370985d28474051193d68b53ff7
sevenlol <sevenlol1007@gmail.com>
2016-05-11 00:18:30 +0800

    [master] Implement about activity

--- app/src/main/java/tw/gov/ey/nici/AboutActivity.java
+++ app/src/main/java/tw/gov/ey/nici/AboutActivity.java
@@ -1,9 +1,17 @@
 package tw.gov.ey.nici;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.RelativeLayout;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -27,10 +35,67 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
         if (actionBar != null) {
             actionBar.setNavigationOnClickListener(this);
         }
+
+        // set about container
+        RelativeLayout aboutContainer = (RelativeLayout) findViewById(R.id.about_container);
+        if (aboutContainer != null) {
+            AboutPage about = new AboutPage(this)
+                    .isRTL(false)
+                    .setImage(R.drawable.nici_logo_full)
+                    .setDescription(getString(R.string.about_description))
+                    .addItem(getVersionElement())
+                    .addGroup(getString(R.string.about_website_group));
+
+            String[] websiteTitleArr = getResources().getStringArray(R.array.about_website_group_title);
+            String[] websiteUrlArr = getResources().getStringArray(R.array.about_website_group_url);
+            if (websiteTitleArr != null && websiteUrlArr != null &&
+                websiteTitleArr.length == websiteUrlArr.length) {
+                for (int i = 0; i < websiteTitleArr.length; i++) {
+                    if (websiteTitleArr[i] == null || websiteUrlArr[i] == null) {
+                        continue;
+                    }
+
+                    about.addItem(getWebsiteElement(websiteTitleArr[i], websiteUrlArr[i]));
+                }
+            }
+
+            aboutContainer.addView(about.create());
+        }
     }
 
     @Override
     public void onClick(View view) {
         finish();
+    }
+
+    private Element getWebsiteElement(String title, String url) {
+        Element element = new Element();
+        element.setTitle(title);
+        element.setIcon(R.drawable.about_icon_link);
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            element.setIntent(intent);
+        } catch (Exception e) {
+            Log.d("About", "Parse Uri Failed: " + url);
+        }
+        return element;
+    }
+
+    private Element getVersionElement() {
+        String version = "";
+        if (getPackageManager() != null && getPackageName() != null) {
+            try {
+                PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), 0);
+                if (info.versionName != null) {
+                    version = info.versionName;
+                }
+            } catch (Exception e) {
+                Log.d("About", "Get Package Info Failed");
+            }
+        }
+        Element element = new Element();
+        element.setTitle(String.format(getString(R.string.about_version_format), version));
+        return element;
     }
 }
